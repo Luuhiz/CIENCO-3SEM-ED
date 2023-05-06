36cd088391ea7e766c999bc2a58eec1af6b74696
Luuhiz <116036278+Luuhiz@users.noreply.github.com>
2023-05-05 21:54:14 -0300

    Add files via upload

---
+++ Aula 28 04/Ex bubble sort 2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimeVetor(int x[])
+{
+    for(int i = 0; i <= 4; i++)
+    {
+        cout << i + 1 << " Numero: " << x[i] << endl;
+    }
+}
+
+void bubbleSort2(int x[])
+{
+    int aux = 0;
+    //ordenando de forma cescente
+    //laço com a quantidade de elementos do vetor - 1
+    for(int j = 1; j <= 4; j++)
+    {
+        for(int i = 4; i >= j;i--)
+        {
+            if(x[i] < x[i - 1])
+
+            {aux = x[i];
+            x[i] = x[i - 1];
+            x[i - 1] = aux;
+            }
+        }
+    }
+
+}
+
+
+
+int main(int argc, char** argv)
+{
+
+   int x[5];
+
+   //carregando os numeros no vetor
+   for(int i =0; i <= 4; i++)
+   {
+       cout << "Digite o numero: ";
+       cin >> x[i];
+   }
+
+   bubbleSort2(x);
+   imprimeVetor(x);
+
+   return 0;
+
+}
